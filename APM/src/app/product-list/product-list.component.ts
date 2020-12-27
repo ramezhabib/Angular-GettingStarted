50bf765b238ae3ac57894afe925e98b22f49f3cb
Ramez Habib <ramezhabib@live.com>
2020-12-28 00:04:49 +0200

    Module 8 Services

--- APM/src/app/product-list/product-list.component.ts
+++ APM/src/app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product/product';
+import { ProductService } from './product/product.service';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit{
   pageTitle = "Product List";
@@ -12,46 +13,22 @@ export class ProductListComponent implements OnInit{
   imageMargin = 2;
   showImage = false;
 
-  products: IProduct[] =
-    [
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2019",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      }
-    ]
+  products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
 
-  filteredProducts: IProduct[];
   _listFilter: string;
-
   get listFilter(): string  {
     return this._listFilter;
   }
-
   set listFilter(value: string) {
     this._listFilter = value;
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
 
-  // constructor() {
-  //   this.filteredProducts = this.products;
-  //   this.listFilter = 'cart';
-  // }
+  constructor(private productService: ProductService) {
+
+    this.listFilter = 'cart';
+  }
 
   toggleImage() {
     this.showImage = !this.showImage;
@@ -68,7 +45,7 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProducts();
   }
 
 
