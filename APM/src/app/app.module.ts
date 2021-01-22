df3c1893f48aba13efc708832d2bdf5bf92be7e1
Ramez Habib <ramezhabib@live.com>
2021-01-22 00:39:08 -0500

    Module 13 Angular Modules

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,43 +1,26 @@
 import { NgModule } from '@angular/core';
-import {FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import {StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './product-list/product/product-detail.component'
 import { WelcomeComponent } from './home/welcome.component'
-
-import { ProductDetailGuard } from './product-list/product/product-detail.guard'
+import { ProductModule } from './product-list/product/product.module';
+import { AppRoutingModule } from './app-routing.module'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     // define external modules
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent  },
-      { path: 'products/:id',
-        canActivate: [ProductDetailGuard ] ,
-        component: ProductDetailComponent },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-
-    ])
+    //ng registers routes based on order of modules specified here
+    ProductModule,
+    AppRoutingModule
   ],
   //defines startup component for app
   bootstrap: [AppComponent]
---
+++ APM/src/app/product-list/product/product.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from '../product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
+  imports: [
+    SharedModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
